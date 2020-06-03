69502f3fc396cc08af919c270f61403367924d72
onichandame <zxinmyth@gmail.com>
2020-06-03 08:07:39 +0000

    export

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
-export * from './withApollo'
+export * from "./withApollo"
 export {
   useQuery,
   useMutation,
   useLazyQuery,
   useSubscription
-} from '@apollo/react-hooks'
+} from "@apollo/react-hooks"
